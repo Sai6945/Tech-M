e9503bfe3286731447022b72f57b1b2c26aa5bcc
Sai6945 <125623480+Sai6945@users.noreply.github.com>
2025-03-24 11:13:23 +0530

    Add files via upload

--- Day-1/ReverseBits.java
+++ Day-1/ReverseBits.java
@@ -1,11 +1,25 @@
+package Day1;
 import java.util.Scanner;
-class ReverseBits {
+public class ReverseBits {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int num = sc.nextInt();
-        int rev = Integer.reverse(num);
-        System.out.println("Reversed Binary: " + Integer.toBinaryString(rev));
-        System.out.println("Equivalent Decimal: " + rev);
+        int num = scanner.nextInt();
+        scanner.close();
+        
+        int reversedNum = reverseBits(num);
+        
+        System.out.println("Reversed Binary: " + Integer.toBinaryString(reversedNum));
+        System.out.println("Equivalent Decimal: " + reversedNum);
+    }
+    
+    public static int reverseBits(int num) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) { // Assuming 32-bit integer
+            result <<= 1; // Left shift result
+            result |= (num & 1); // Get the last bit of num and set it in result
+            num >>= 1; // Right shift num
+        }
+        return result;
     }
 }
